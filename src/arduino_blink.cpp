8f8410674391c8f0cd4447aa41b4f92c60133096
Dieter Niklaus <dieter.niklaus@gmx.net>
2014-07-01 20:37:47 +0200

    - NEW: added example implementation for arduino_blink.cpp - CHG: updated workspace/.metadata.zip

--- src/arduino_blink.cpp
+++ src/arduino_blink.cpp
@@ -1,15 +1,33 @@
 // Do not remove the include below
 #include "arduino_blink.h"
 
+#include "Timer.h"
+
+void toggleLed(int ledPin)
+{
+  bool isLedOn = digitalRead(ledPin);
+  digitalWrite(ledPin, !isLedOn);
+}
+
+const unsigned int  BLINK_TIME_MILLIS = 200;
+class BlinkTimerAdapter : public TimerAdapter
+{
+public:
+  void timeExpired()
+  {
+    toggleLed(LED_BUILTIN);
+  }
+};
 
 //The setup function is called once at startup of the sketch
 void setup()
 {
-// Add your initialization code here
+  pinMode(LED_BUILTIN, OUTPUT);
+  new Timer(new BlinkTimerAdapter(), Timer::IS_RECURRING, BLINK_TIME_MILLIS);
 }
 
 // The loop function is called in an endless loop
 void loop()
 {
-//Add your repeated code here
+  yield();
 }
