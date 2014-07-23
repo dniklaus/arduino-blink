174ff48aa46bcb2241a2c5cad860192ef8ff3532
Dieter Niklaus <dieter.niklaus@gmx.net>
2014-07-23 19:11:15 +0200

    - FIX: src/arduino_blink.cpp: reverted the changes made by mistake (the exercise was already  solved... :-)
    Signed-off-by: Dieter Niklaus <dieter.niklaus@gmx.net>

--- src/arduino_blink.cpp
+++ src/arduino_blink.cpp
@@ -1,33 +1,15 @@
 // Do not remove the include below
 #include "arduino_blink.h"
 
-#include "Timer.h"
-
-void toggleLed(int ledPin)
-{
-  bool isLedOn = digitalRead(ledPin);
-  digitalWrite(ledPin, !isLedOn);
-}
-
-const unsigned int  BLINK_TIME_MILLIS = 200;
-class BlinkTimerAdapter : public TimerAdapter
-{
-public:
-  void timeExpired()
-  {
-    toggleLed(LED_BUILTIN);
-  }
-};
 
 //The setup function is called once at startup of the sketch
 void setup()
 {
-  pinMode(LED_BUILTIN, OUTPUT);
-  new Timer(new BlinkTimerAdapter(), Timer::IS_RECURRING, BLINK_TIME_MILLIS);
+  // Add your initialization code here
 }
 
 // The loop function is called in an endless loop
 void loop()
 {
-  yield();
+  // Add your repeated code here
 }
